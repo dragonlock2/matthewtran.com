064ebdfe1bb9d4b49e3a849987685fe4da6822c2
Matthew Tran <matthewlamtran@berkeley.edu>
2023-10-28 21:02:12 +0000

    update monerod v0.18.3.1

--- monerod/entry.sh
+++ monerod/entry.sh
@@ -7,7 +7,7 @@ monero/monerod \
     --restricted-rpc \
     --zmq-pub tcp://0.0.0.0:18083 \
     --out-peers 64 --in-peers 32 \
-    --add-priority-node=node.supportxmr.com:18080 \
+    --add-priority-node=p2pmd.xmrvsbeast.com:18080 \
     --add-priority-node=nodes.hashvault.pro:18080 \
     --disable-dns-checkpoints \
     --enable-dns-blocklist
