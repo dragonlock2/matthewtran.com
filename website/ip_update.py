8dc472a9c82cec25a99277d3776e9a32d433d882
Matthew Tran <matthewlamtran@berkeley.edu>
2024-03-08 05:40:32 +0000

    update ip script

--- website/ip_update.py
+++ website/ip_update.py
@@ -11,13 +11,18 @@ if __name__ == '__main__':
 
     old_ipv4, old_ipv6 = None, None
     while True:
-        ipv4 = urllib.request.urlopen('https://v4.ident.me').read().decode('utf8')
-        # ipv6 = urllib.request.urlopen('https://v6.ident.me').read().decode('utf8')
-        ipv6 = None
+        try:
+            ipv4 = urllib.request.urlopen('https://v4.ident.me').read().decode('utf8')
+            # ipv6 = urllib.request.urlopen('https://v6.ident.me').read().decode('utf8')
+            ipv6 = None
+        except Exception as e:
+            print(e)
+            time.sleep(60)
+            continue
 
         if ipv4 != old_ipv4 or ipv6 != old_ipv6:
             msg = Mail(
-                from_email='matthewlamtran@berkeley.edu',
+                from_email='mtran319@gmail.com',
                 to_emails='mtran319@gmail.com',
                 subject='pls update ip',
                 html_content=f'<p>ipv4: {ipv4}</p><p>ipv6: {ipv6}</p>'
@@ -26,8 +31,10 @@ if __name__ == '__main__':
                 print(f'IP changed to {ipv4} and {ipv6}')
                 resp = sg.send(msg)
             except Exception as e:
-                print(e.message)
+                print(e)
                 sg = SendGridAPIClient(Path('sendgrid.key').read_text())
+                time.sleep(60)
+                continue
 
         old_ipv4, old_ipv6 = ipv4, ipv6
-        time.sleep(60 * 60) # 60 min
+        time.sleep(3600)
