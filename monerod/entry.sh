ee115a43f2d9f7edcce648ccd6a89d1fdcfc603b
Matthew Tran <matthewlamtran@berkeley.edu>
2024-09-17 11:36:35 +0000

    monerod graceful stop

--- monerod/entry.sh
+++ monerod/entry.sh
@@ -3,9 +3,9 @@
 # check bitmonero.log for log
 monero/monerod \
     --prune-blockchain \
-    --rpc-bind-ip 0.0.0.0 \
-    --confirm-external-bind \
-    --restricted-rpc \
+    --rpc-bind-port 18089 \
+    --rpc-restricted-bind-ip 0.0.0.0 \
+    --rpc-restricted-bind-port 18081 \
     --zmq-pub tcp://0.0.0.0:18083 \
     --out-peers 32 --in-peers 64 \
     --add-priority-node=p2pmd.xmrvsbeast.com:18080 \
@@ -15,7 +15,7 @@ monero/monerod \
     --detach
 
 cleanup() {
-    monero/monerod exit
+    monero/monerod exit --rpc-bind-port 18089
 }
 trap 'cleanup' TERM
 tail -f /dev/null &
