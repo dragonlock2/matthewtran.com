e0cf8396215c7d00a389a72fe2471c6cd32146b6
Matthew Tran <matthewlamtran@berkeley.edu>
2025-05-16 18:16:00 -0700

    remove priority nodes, causing drops

--- monerod/entry.sh
+++ monerod/entry.sh
@@ -9,8 +9,6 @@ monero/monerod \
     --rpc-restricted-bind-port 18081 \
     --zmq-pub tcp://0.0.0.0:18083 \
     --out-peers 32 --in-peers 64 \
-    --add-priority-node=p2pmd.xmrvsbeast.com:18080 \
-    --add-priority-node=nodes.hashvault.pro:18080 \
     --disable-dns-checkpoints \
     --enable-dns-blocklist \
     --detach
